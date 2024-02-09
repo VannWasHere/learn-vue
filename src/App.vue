0ee52639cc218414c1e28f462c89eb701857effa
VannWasHere <ateen1203@gmail.com>
2024-02-09 16:10:15 +0700

    Adding Simple Component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <NavigationBar/>
+  </header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavigationBar from './components/NavigationBar.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: #2b2e31;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #d9d9d9;
 }
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,94 @@
+<template>
+    <nav class="navigation">
+        <section class="left-navbar">
+            <div id="image-container">
+                <img src="../assets/logo.png" id="nav-image" alt="Vue Logo">
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Product</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+        </section>
+        <section class="right-navbar">
+            <div class="profile-section">
+                <h2><a href="">Justine Donovan</a></h2>
+            </div>
+            <button id="sign-out-button">Sign Out</button>
+        </section>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavigationBar'
+}
+</script>
+
+<style scoped>
+    .navigation {
+        display: flex;
+        justify-content: space-between;
+        padding: 15px;
+        background-color: rgb(17, 15, 15);
+    }
+    .left-navbar {
+        display: flex;
+        width: 100%;
+        align-items: center;
+    }
+    #image-container {
+        width: 50px;
+        height: 50px;
+    }
+    #nav-image {
+        width: 100%
+    }
+    .nav-list {
+        padding-left: 1rem;
+        padding-right: 1rem;
+        margin: 10px;
+        width: 20%;
+        font-size: 17px;
+        margin-left: 30px;
+    }
+    .nav-list ul {
+        display: flex;
+        justify-content: space-between;
+    }
+    .nav-list ul>li{
+        list-style: none;
+    }
+    .nav-list ul>li a {
+        text-decoration: none;
+        color: #d9d9d9;
+    }
+    .right-navbar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: fit-content;
+        text-align: right;
+        gap: 12px;
+    }
+    h2 {
+        font-size: 18px;
+    }
+    .profile-section h2 a{
+        text-decoration: none;
+        color: #d9d9d9;
+    }
+    #sign-out-button {
+        cursor: pointer;
+        width: 80px;
+        padding: 8px;
+        border-radius: 8px;
+        background-color: red;
+        border:none;
+        color: #d9d9d9;
+        font-weight: bold;
+    }
+</style>
